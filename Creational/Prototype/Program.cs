29e19d3b0d7c901c3455a259c90903a7f94fcfae
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-08-13 21:25:34 +0300

    Implement Prototype design pattern

---
+++ Creational/Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Prototype;
+
+internal class Program
+{
+    static void Main()
+    {
+        var original = new Sheep("Jolly", "Mountain Sheep");
+
+        Console.WriteLine(original.Name);
+        Console.WriteLine(original.Category);
+
+        original.Child = new Sheep("Holly", "Mountain Sheep");
+
+        var cloned = original.Clone();
+        cloned.Name = "Dolly";
+        Console.WriteLine(cloned.Name);
+        Console.WriteLine(cloned.Category);
+        Console.WriteLine(original.Name);
+    }
+}
---
+++ Creational/Prototype/Sheep.cs
@@ -0,0 +1,19 @@
+﻿namespace Prototype;
+
+internal class Sheep
+{
+    public Sheep(string name, string category)
+    {
+        this.Name = name;
+        this.Category = category;
+    }
+
+    public string Name { get; set; }
+
+    public string Category { get; set; }
+
+    public Sheep Child { get; set; }
+
+    public Sheep Clone()
+        => MemberwiseClone() as Sheep;
+}
