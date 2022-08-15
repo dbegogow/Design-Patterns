df5bf71a432313fe99811295f8b795576f664763
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-08-15 23:11:47 +0300

    Implement Singleton design pattern

---
+++ Creational/Singleton/President.cs
@@ -0,0 +1,20 @@
+﻿namespace Singleton;
+
+internal class President
+{
+    private static President instance;
+
+    private President()
+    {
+    }
+
+    public static President GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new President();
+        }
+
+        return instance;
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton;
+
+internal class Program
+{
+    static void Main()
+    {
+        var a = President.GetInstance();
+        var b = President.GetInstance();
+
+        Console.WriteLine(a == b);
+    }
+}
